285dceeec6fcf61ad1870c0eccc2d77ed1957f68
Omkar Kirpan <okirpan@gmail.com>
2022-07-23 23:20:50 +0530

    refactor: :technologist: indentation fix

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -1,19 +1,19 @@
 import Head from 'next/head'
-import {Box, Container} from '@chakra-ui/react'
+import { Box, Container } from '@chakra-ui/react'
 
-const Main = ({children, router}) => {
-    return (
-        <Box as="main" pb={8}>
-            <Head>
-                <meta name="viewport" contents="width=device-width, initial-scale=1" />
-                <title>Omkar Kirpan - Homepage</title>
-            </Head>
+const Main = ({ children, router }) => {
+  return (
+    <Box as="main" pb={8}>
+      <Head>
+        <meta name="viewport" contents="width=device-width, initial-scale=1" />
+        <title>Omkar Kirpan - Homepage</title>
+      </Head>
 
-            <Container maxW="container.md" pd={14}>
-                {children}
-            </Container>
-        </Box>
-    )
+      <Container maxW="container.md" pd={14}>
+        {children}
+      </Container>
+    </Box>
+  )
 }
 
 export default Main
