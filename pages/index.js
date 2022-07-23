9b836a06205633dd3aeaaf8967ae6ce0b0748749
Omkar Kirpan <okirpan@gmail.com>
2022-07-23 22:56:02 +0530

    feat: :sparkles: integrate with Chakra UI

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,21 @@
+import { Container, Box, Heading } from '@chakra-ui/react'
 const Page = () => {
-    return <div>Hello</div>
+    return (
+        <Container>
+            <Box borderRadius="lg" bg="red" p={3} mb={6} align="center">
+                Hello, I&apos;m a full-stack developer based in India!
+            </Box>
+
+            <Box display={{md:'flex'}}>
+                <Box flexGrow={1}>
+                    <Heading as="h2" variant="page-title">
+                        Omkar Kirpan
+                    </Heading>
+                    <p>Digital Craftzman ( Artist / Developer / Designer )</p>
+                </Box>
+            </Box>
+        </Container>
+    )
 } 
 
 export default Page
